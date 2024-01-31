20e143af7442930bf19fe5ef629d5736413c2350
shekhar16 <shekhar16@users.noreply.github.com>
2024-02-01 01:08:55 +0530

    feat: add ClientDuoTest.java

---
+++ projects/username-pwd-Auth/lib/io/jans/duo/ClientDuoTest.java
@@ -0,0 +1,32 @@
+package com.duosecurity;
+
+import com.duosecurity.exception.DuoException;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class ClientDuoTest {
+
+    private Client duoClient;
+
+    public String  duoValidate() throws DuoException {
+        Map<String, String> stateMap = new HashMap<>();;
+        String CLIENT_ID = "DI1QCWC6TY96FLSPDEKE";
+        String CLIENT_SECRET = "sPqo9w4BgBOJexIwJd105ZEzaLeoqEB2HaunMKLF";
+        String API_HOST = "api-de9a3a97.duosecurity.com";
+        String HTTPS_REDIRECT_URI = "https://shekhar16-dynamic-crab.gluu.info/duo-callback";
+        this.duoClient = new Client.Builder(CLIENT_ID, CLIENT_SECRET, API_HOST, HTTPS_REDIRECT_URI).build();
+
+        String state = duoClient.generateState();
+        // Store the state to remember the session and username
+        String username = "admin";
+        stateMap.put(state, username);
+
+        // Step 4: Create the authUrl and redirect to it
+        String authUrl = duoClient.createAuthUrl(username, state);
+
+        return authUrl;
+
+    }
+
+}
