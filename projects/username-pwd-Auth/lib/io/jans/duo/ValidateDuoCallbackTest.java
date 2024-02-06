bced4debb1fa45f0473ef09781a19b86120418d9
shekhar16 <shekhar16@users.noreply.github.com>
2024-02-06 18:39:34 +0530

    feat: update ValidateDuoCallbackTest.java

--- projects/username-pwd-Auth/lib/io/jans/duo/ValidateDuoCallbackTest.java
+++ projects/username-pwd-Auth/lib/io/jans/duo/ValidateDuoCallbackTest.java
@@ -3,7 +3,7 @@ package io.jans.duo;
 class ValidateDuoCallbackTest {
 
     public static String validateCallback(){
-        System.out.println("check callbackUrl  :  " + callbackUrl);
+        System.out.println("check callbackUrl  :  ");
         return "success";
     }
 
