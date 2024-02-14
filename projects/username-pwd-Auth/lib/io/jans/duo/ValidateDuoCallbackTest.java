78fb1de36a85a0b93252fe26e377bd2b9b358ab1
shekhar16 <shekhar16@users.noreply.github.com>
2024-02-15 02:10:25 +0530

    feat: update ValidateDuoCallbackTest.java

--- projects/username-pwd-Auth/lib/io/jans/duo/ValidateDuoCallbackTest.java
+++ projects/username-pwd-Auth/lib/io/jans/duo/ValidateDuoCallbackTest.java
@@ -1,10 +1,48 @@
 package io.jans.duo;
 
+
+import com.duosecurity.Client;
+import com.duosecurity.exception.DuoException;
+import com.duosecurity.model.Token;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.Map;
+
 class ValidateDuoCallbackTest {
 
-    public static String validateCallback(Map callbackDetails){
-        System.out.println("check callbackUrl  :  " + callbackDetails);
-        return callbackDetails;
+    private static Client duoClient;
+
+    public static String validateCallback(Map callbackUrl, String uid) throws DuoException {
+        System.out.println("check callbackUrl  :  " + callbackUrl);
+        String state = (String) callbackUrl.get("state");
+        String duoCode = (String) callbackUrl.get("duo_code");
+        System.out.println("state : " + state + " duoCode : " + duoCode + "uid" + uid);
+        Token token = duoClient.exchangeAuthorizationCodeFor2FAResult(duoCode, uid);
+        String result = "false";
+        // If the auth was successful, render the welcome page otherwise return an error
+        if (authWasSuccessful(token)) {
+            result = tokenToJson(token);
+        }
+        return result;
     }
+
+    private static boolean authWasSuccessful(Token token) {
+        if (token != null && token.getAuth_result() != null) {
+            return "ALLOW".equalsIgnoreCase(token.getAuth_result().getStatus());
+        }
+        return false;
+    }
+
+    private static String tokenToJson(Token token) throws DuoException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            return objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(token);
+        } catch (JsonProcessingException jpe) {
+            throw new DuoException("Could not convert token to JSON");
+        }
+    }
+
+}
 
 }
