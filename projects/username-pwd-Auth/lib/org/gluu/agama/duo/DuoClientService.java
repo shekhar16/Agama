67156b03c644b33371a98a5d33b4589256fafdd3
shekhar16 <shekhar16@users.noreply.github.com>
2024-03-02 09:06:57 +0000

    feat: update DuoClientService.java

--- projects/username-pwd-Auth/lib/org/gluu/agama/duo/DuoClientService.java
+++ projects/username-pwd-Auth/lib/org/gluu/agama/duo/DuoClientService.java
@@ -30,9 +30,40 @@ class DuoClientService{
 
     }
 
-    public static String validateCallback(String callbackUrl){
+    public static String validateCallback(Map callbackUrl, String uid) throws DuoException {
+        String CLIENT_ID = "DI1QCWC6TY96FLSPDEKE";
+        String CLIENT_SECRET = "sPqo9w4BgBOJexIwJd105ZEzaLeoqEB2HaunMKLF";
+        String API_HOST = "api-de9a3a97.duosecurity.com";
+        String HTTPS_REDIRECT_URI = "https://shekhar16-prepared-donkey.gluu.info/jans-auth/fl/callback";
+        this.duoClient = new Client.Builder(CLIENT_ID, CLIENT_SECRET, API_HOST, HTTPS_REDIRECT_URI).build();
+
         System.out.println("check callbackUrl  :  " + callbackUrl);
-        return callbackUrl;
+        String state = (String) callbackUrl.get("state");
+        String duoCode = (String) callbackUrl.get("duo_code");
+        System.out.println("state : " + state + " duoCode : " + duoCode + "uid" + uid);
+        Token token = duoClient.exchangeAuthorizationCodeFor2FAResult(duoCode, uid);
+        String result = "false";
+        // If the auth was successful, render the welcome page otherwise return an error
+        if (authWasSuccessful(token)) {
+            result = tokenToJson(token);
+        }
+        return result;
+    }
+
+    private static boolean authWasSuccessful(Token token) {
+        if (token != null && token.getAuth_result() != null) {
+            return "ALLOW".equalsIgnoreCase(token.getAuth_result().getStatus());
+        }
+        return false;
+    }
+
+    private static String tokenToJson(Token token) throws DuoException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            return objectMapper.writer().writeValueAsString(token);
+        } catch (JsonProcessingException jpe) {
+            throw new DuoException("Could not convert token to JSON");
+        }
     }
 
 }
